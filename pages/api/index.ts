85d5502ffd0ab149fbb7f6498b8b328473b25877
l1ttps <l1ttps443@gmail.com>
2022-12-05 17:46:58 +0700

    test: foward api

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import getHomeSelection from "../../api/services/getHomeSelection";
+import { HomeSection } from "../../shared/types";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<HomeSection[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const homeData = await getHomeSelection();
+  res.status(200).json(homeData);
 }
